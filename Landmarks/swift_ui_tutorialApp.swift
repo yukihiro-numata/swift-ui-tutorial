457473be0c64702d209150c6687307232b90fc37
yukihiro-numata <yuki.numata97@gmail.com>
2022-02-25 16:20:03 +0900

    Add step3.

--- Landmarks/swift_ui_tutorialApp.swift
+++ Landmarks/swift_ui_tutorialApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct swift_ui_tutorialApp: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
